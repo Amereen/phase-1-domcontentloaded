8502323eee7e94e4748654641e3fea51f118491a
Amereen Hanvel <amereenhanvel786@gmail.com>
2022-09-09 16:19:22 -0400

    commit tree af12e491555ec3682c52291e6493d5f308fdcb91

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', ()=>{
+  console.log('After DOM Loaded')
+  // document.body.innerHTML = "This is really cool!";
+  document.getElementById("text").textContent="This is really cool!"
+})
+
